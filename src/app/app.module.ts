7b6ae10901736743fd53dd58266493a5001a5600
dymafr <erwan1309@hotmail.com>
2021-10-05 11:47:05 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,12 @@ import { HomeComponent } from './views/home/home.component';
 import { ConnexionComponent } from './views/connexion/connexion.component';
 import { InscriptionComponent } from './views/inscription/inscription.component';
 import { ProfilComponent } from './views/profil/profil.component';
+import { StoreModule } from '@ngrx/store';
+import { ROOT_REDUCERS } from './shared/store';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -30,6 +36,13 @@ import { ProfilComponent } from './views/profil/profil.component';
     ReactiveFormsModule,
     HttpClientModule,
     LayoutModule,
+    StoreModule.forRoot(ROOT_REDUCERS),
+    StoreRouterConnectingModule.forRoot(),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
